bfed998cfcf08ed5fdcb9ca2ddb655b12028b181
QuickOrBeDead <boraakgn@gmail.com>
2023-01-14 18:10:50 +0300

    add cors and map notify-clients endpoint

--- Scenario1/NotificationApi/Program.cs
+++ Scenario1/NotificationApi/Program.cs
@@ -1,11 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddCors();
 
 var app = builder.Build();
+app.UseCors(
+    b =>
+        {
+            b.WithOrigins("http://localhost:8080")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .AllowCredentials();
+        });
 
 app.MapHub<NotificationHub>("/notificationHub");
+app.MapPost("/notify-clients", (HttpContext httpContext, [FromBody]decimal price) =>
+    {
+        var hubContext = httpContext.RequestServices.GetRequiredService<IHubContext<NotificationHub>>();
+        return hubContext.Clients.All.SendAsync("ReceivePrice", price);
+    });
 app.Run();
 
 public class NotificationHub : Hub
