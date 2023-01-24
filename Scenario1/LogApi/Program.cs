4ece3ce5e28a7444789b6114b3eafc3fd5fe610f
QuickOrBeDead <boraakgn@gmail.com>
2023-01-24 21:30:30 +0300

    add ElasticClient and implement add method

--- Scenario1/LogApi/Program.cs
+++ Scenario1/LogApi/Program.cs
@@ -1,32 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Nest;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IElasticClient>(_ => new ElasticClient(new ConnectionSettings(new Uri("http://elasticsearch:9200"))
+    .BasicAuthentication("elastic", "Password1")
+    .DefaultIndex("request")
+    .DefaultMappingFor<RequestInfo>(x => x.IdProperty(y => y.RequestId).IndexName("request"))));
+builder.Services.AddCors();
 
 var app = builder.Build();
+app.UseCors(
+    b =>
+        {
+            b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
 
 // Configure the HTTP request pipeline.
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapPost(
+    "Add",
+    (HttpContext httpContext, [FromBody]RequestInfo requestInfo) =>
+        {
+            var elasticClient = httpContext.RequestServices.GetRequiredService<IElasticClient>();
+            elasticClient.IndexDocumentAsync(requestInfo);
+            return Results.Ok();
+        });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+internal sealed record RequestInfo(string RequestId, DateTime StartTime, DateTime EndTime, int DurationInMs);
